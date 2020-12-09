db8e3785929544cd3ab623f50a341a43fbc80af7
y-noda <y-noda@entaworks.co.jp>
2020-12-09 21:18:18 +0900

    111

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const CategoriesScreen = props => {
+const CategoryMealsScreen = props => {
   return (
     <View style={styles.screen} >
-      <Text>The Categories Screen!</Text>
+      <Text>The Category Meal Screen!</Text>
     </View>
   );
 };
@@ -17,4 +17,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default CategoriesScreen;
+export default CategoryMealsScreen;

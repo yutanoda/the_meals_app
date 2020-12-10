704b33dfa925f3310cc4bec5be61ea054ccb6c40
y-noda <y-noda@entaworks.co.jp>
2020-12-10 11:01:11 +0900

    115の終わり

---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,18 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
+import CategoriesScreen from '../screens/CategoriesScreen';
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import MealDetailScreen from '../screens/CategoryMealsScreen';
+
+const MealsNavigator = createStackNavigator({
+  Categories: CategoriesScreen,
+  CategoryMeals: {
+    screen: CategoryMealsScreen,
+  },
+  MealDetail: MealDetailScreen
+});
+
+export default createAppContainer(MealsNavigator);
